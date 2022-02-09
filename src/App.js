c113685a40d8b5aca2368a5c778d412fef82cfb1
Askarunro <paskar.y.a@gmail.com>
2022-02-09 19:52:47 +0200

    add

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+import AppBar from './components/AppBar/AppBar';
+
+const navLink = [
+  {
+    href: "/univesity",
+    text: "university",
+  },
+  {
+    href: "/univesity",
+    text: "university",
+  }
+];
+
+
+function App() {
+    return <AppBar />;
+}
+
+export default App;
---
+++ src/components/AppBar/AppBar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+const AppBar = () =>{
+    return (
+        <div>
+        <h2>AppBar</h2>
+        </div>
+    )
+   
+};
+
+export default AppBar
